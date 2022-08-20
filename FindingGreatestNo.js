e84166b53738f748fe67139e3050b279b762c5bd
vidhya <vidhya.raghav21@gmail.com>
2022-08-20 17:52:02 +0530

    added another program related files

---
+++ FindingGreatestNo.js
@@ -0,0 +1,25 @@
+function findingGreatestNumber()
+{
+   const numbersCount = prompt('How many numbers are to be compared?');
+   const  numberList = [];
+   const copyNumberList = [];
+    for (let counter = 0; counter<= numbersCount-1;counter++)
+    {
+        const userResponse = prompt('Enter the number' + counter+1);
+        numberList.push(userResponse);
+        copyNumberList.push(userResponse);
+    }
+//alert(Math.max(numberList));
+
+
+    for (let counter=1; counter<=numbersCount-1;counter++)
+    {
+        if (numberList[0]< numberList[counter])
+        {
+            numberList[0] = numberList[counter];
+        }
+    }
+   alert(copyNumberList);
+alert('The greatest number entered is ' + numberList[0]);
+
+}
