e388da34b2b7ad4d993e60954f33716b363c70b7
vidhya <vidhya.raghav21@gmail.com>
2022-09-18 18:18:28 +0530

    thirukkural code and css for mobile first

--- assets/JS/thirukkural.js
+++ assets/JS/thirukkural.js
@@ -1,4 +1,5 @@
 const thirukkural = {
+    /*
     kuralDisplayTamil : document.querySelector('.kuralDisplayT'),
     kuralDisplayEnglish : document.querySelector('.kuralDisplayE'),
     chapterNameT : document.querySelector('.spnChNameT'),
@@ -7,12 +8,15 @@ const thirukkural = {
     chapterGroupE : document.querySelector('.spnChGroupE'),
     sectionT : document.querySelector('.spnSectionT'),
     sectionE : document.querySelector('.spnSectionE'),
-    getKuralbyNo : function(){
+    */
+    getKuralbyNo : function(number){
+        this.fetchThirukural(number);
 
     },
     getRandomKural : function(){
        // console.log("clcik");
-        let randomNo =  Math.floor(Math.random() * 134);
+       // total 1330 kural 
+        let randomNo =  Math.floor(Math.random() * 1331);
         console.log(randomNo);
         this.fetchThirukural(randomNo);
     },
@@ -20,18 +24,34 @@ const thirukkural = {
         fetch('https://api-thirukkural.vercel.app/api?num=' + number + ';')
         .then(response => response.json())
         .then(result => {
-        console.log('Thirukural #', result.number)
-        console.log('Thirukural in tamil', result.line1)
-        this.kuralDisplayTamil.innerHTML = result.line1 +'<br>' + result.line2;
-       // alert(this.kuralDisplayTamil.innerHTML);
-       this.chapterNameT.innerHTML = result.chap_tam;
-       this.chapterGroupT.innerHTML = result.chapgrp_tam;
-       this.sectionT.innerHTML = result.sect_tam;
-       this.kuralDisplayEnglish.innerHTML = result.eng;
-       this.chapterNameE.innerHTML = result.chap_eng;
-       this.chapterGroupE.innerHTML = result.chapgrp_Eng;
-       this.sectionE.innerHTML = result.sect_eng;
+       // console.log('Thirukural #', result.number)
+       // console.log('Thirukural in tamil', result.line1)
 
+           this.updateUI(result);
         })
+        },
+        updateUI : function(result){
+            // Tamil kural display
+            document.querySelector('.kuralDisplayT').innerHTML = result.line1 +'<br>' + result.line2;
+            if ( document.querySelector('.spnChNameT').innerHTML !='null')
+            {
+            document.querySelector('.spnChNameT').innerHTML ='அதிகாரம்: '+ result.chap_tam;
+            document.querySelector('.spnChGroupT').innerHTML = 'இயல்: ' + result.chapgrp_tam;
+            document.querySelector('.spnSectionT').innerHTML = 'பால் : '+ result.sect_tam;
+
+            document.querySelector('.spnChNameE').innerHTML = 'Chapter : '+ result.chap_eng;
+            document.querySelector('.spnChGroupE').innerHTML = 'Group: ' +result.chapgrp_eng;
+            document.querySelector('.spnSectionE').innerHTML ='Type: '+ result.sect_eng;
+            }
+            document.querySelector('.spnMeaningT').innerHTML ='பொருள் : '+ result.tam_exp;
+            //result number display
+            document.querySelector('.kuralnumber').innerHTML = result.number;
+            
+            // English kural display
+            document.querySelector('.kuralDisplayE').innerHTML = result.eng;
+           
+            document.querySelector('.spnMeaningE').innerHTML ='Meaning :' + result.eng_exp;
+
         }
+
 }
